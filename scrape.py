67c177acf7d9c0a8bc7ad139d6f79b5cd6af8972
Kathy <kchen2013@gmail.com>
2015-08-12 17:23:24 -0400

    code cleanup, refactoring

--- scrape.py
+++ scrape.py
@@ -1,20 +1,23 @@
 import urllib2
 import argparse
 import re
+
 BASE_URL = 'http://www.yelp.com/biz/'
 DEFAULT_LOCATION = 'new-york'
 DEFAULT_BUSINESS = 'saigon-shack'
 DEFAULT_Q = 'pho'
+
 def main():
 	parser = argparse.ArgumentParser()
 	parser.add_argument('-b', '--business', dest='business', default=DEFAULT_BUSINESS, type=str, help='Search for this Business ID (default: %(default)s)')
 	parser.add_argument('-l', '--loc', dest='loc', default=DEFAULT_LOCATION, type=str, help='Search in this location (default: %(default)s)')
 	parser.add_argument('-q', '--q', dest='q', default=DEFAULT_Q, type=str, help='Query (default: %(default)s)')
 	values = parser.parse_args()
-	q = values.q
 	starting_url = BASE_URL + values.business + '-' + values.loc
-	fst_page = starting_url + '?q=' + q
-	print fst_page
+	stats = calculate_avg_rating(starting_url, values.q)
+	print str(stats['total']) + " reviews mention " + values.q + " with average rating " + str(stats['avg'])
+
+def get_pages_of_reviews(fst_page):
 	page = urllib2.urlopen(fst_page)
 	html = page.read()
 	found = html.find(' reviews mentioning') - 1
@@ -26,12 +29,17 @@ def main():
 		check_num = html[found]
 
 	pages = (int(total_reviews) / 40) + 1
+	return pages
+
+def calculate_avg_rating(starting_url, query):
+	fst_page = fst_page = starting_url + '?q=' + query
+	print fst_page
+	pages = get_pages_of_reviews(fst_page)
 	total_ratings = 0
 	count = 0
-	
 	for i in range(0, pages):
 		start = 'start=' + str(i * 40)
-		whole_url = starting_url + '?' + start + '&q=' + q
+		whole_url = starting_url + '?' + start + '&q=' + query
 		whole_page = urllib2.urlopen(whole_url)
 		whole_html = whole_page.read()
 		start_here = whole_html.find('Recommended Reviews')
@@ -44,6 +52,7 @@ def main():
 			count += 1
 			total_ratings += int(rating)
 	avg_rating = float(total_ratings) / count
-	print str(count) + " reviews mention " + q + " with average rating " + str(avg_rating) 
+	return {'total': count, 'avg': avg_rating }
+
 if __name__ == '__main__':
 	main()
